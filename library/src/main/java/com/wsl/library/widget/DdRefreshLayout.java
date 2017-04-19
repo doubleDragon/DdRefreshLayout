1e9a441161bb923d38531be049b39edb4708db13
doubleDragon <aiai373824745@gmail.com>
2017-04-19 22:06:43 +0800

    Second commit

--- library/src/main/java/com/wsl/library/widget/DdRefreshLayout.java
+++ library/src/main/java/com/wsl/library/widget/DdRefreshLayout.java
@@ -1,39 +1,49 @@
 package com.wsl.library.widget;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.view.NestedScrollingParent;
-import android.support.v4.view.NestedScrollingParentHelper;
-import android.support.v4.view.ViewCompat;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 
 /**
- * Created by wsl on 17/4/18.
+ * 在NestedLayout基础上增加各种状态
+ * Created by wsl on 17/4/19.
  */
 
-public class DdRefreshLayout extends ViewGroup implements NestedScrollingParent {
+public class DdRefreshLayout extends DdNestedLayout {
 
     private static final String TAG = DdRefreshLayout.class.getSimpleName();
 
-    @NonNull
-    private View mContentView;
-    @Nullable
-    private View mHeaderView;
-    private int mHeaderHeight;
-    @Nullable
-    private View mFooterView;
-    private int mFooterHeight;
+    //初始状态
+    private static final int STATE_ORIGIN = 0;
 
-    private DdScrollerHelper mScrollerHelper;
-    private NestedScrollingParentHelper mNestedScrollingParentHelper;
-    private boolean mSkipNestedPreScroll;
+    //正在下拉
+    private static final int STATE_PULL_DRAG = 1;
+    //下拉松手后偏移量达到下拉刷新条件，DRAG->WAIT
+    private static final int STATE_PULL_RETURN = 2;
+    //下拉完成后，正在刷新
+    private static final int STATE_PULL_WAIT = 3;
+    //下拉刷新完成，WAIT->ORIGIN
+    private static final int STATE_PULL_RESET = 4;
+    //下拉刷新的2种情况分别是:0->1->0,  0-1-2-3-4-0
 
-    private boolean mChildFirstItemFullVisible;
-    private boolean mChildLastItemFullVisible;
+    //正在上拉
+    private static final int STATE_LOAD_DRAG = -1;
+    //上拉松手后偏移量达到加载更多条件，DRAG->WAIT
+    private static final int STATE_LOAD_RETURN = -2;
+    //上拉完成后，正在加载更多
+    private static final int STATE_LOAD_WAIT = -3;
+    //加载完成，回滚状态
+    private static final int STATE_LOAD_RESET = -4;
+    //上拉加载更多的2种情况分别是:0->(-1)->0,  0->(-1)->(-2)->(-3)->(-4)->0
+
+
+    //下拉刷新的临界值,必须大于或者等于header高度
+    private int mPullCriticalValue;
+    //上拉加载的临界值,必须大于或者等于footer高度
+    private int mLoadCriticalValue;
+    private int mState = STATE_ORIGIN;
 
     public DdRefreshLayout(Context context) {
         this(context, null);
@@ -46,172 +56,99 @@ public class DdRefreshLayout extends ViewGroup implements NestedScrollingParent 
     public DdRefreshLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-
-        mScrollerHelper = new DdScrollerHelper(this);
-        mNestedScrollingParentHelper = new NestedScrollingParentHelper(this);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DdRefreshLayout);
+        mPullCriticalValue = a.getInt(R.styleable.DdRefreshLayout_dd_pull_critical, 0);
+        mLoadCriticalValue = a.getInt(R.styleable.DdRefreshLayout_dd_load_critical, 0);
+        a.recycle();
     }
 
-    @Override
-    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
-        mChildFirstItemFullVisible = !ViewCompat.canScrollVertically(target, -1);
-        if(!mChildFirstItemFullVisible) {
-            mChildLastItemFullVisible = !ViewCompat.canScrollVertically(target, 1);
-        }
-//        Log.d(TAG, "onStartNestedScroll firstItemFullVisible =" + mChildFirstItemFullVisible + "---lastItemFullVisible: " + mChildLastItemFullVisible);
-
-//        boolean canScrollDown = ViewCompat.canScrollVertically(target, -1);
-//        boolean canScrollUp = ViewCompat.canScrollVertically(target, 1);
-//        Log.d(TAG, "onStartNestedScroll canScrollDown =" + canScrollDown + "---canScrollUp: " + canScrollUp);
-
-
-        boolean started = (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0
-                && (mChildFirstItemFullVisible || mChildLastItemFullVisible);
-
-        Log.d(TAG , "onStartNestedScroll ---started: " + started);
-
-        return started;
-    }
-
-    @Override
-    public void onNestedScrollAccepted(View child, View target, int axes) {
-        super.onNestedScrollAccepted(child, target, axes);
-        mNestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes);
-    }
-
-    @Override
-    public void onNestedPreScroll(View child, int dx, int dy, int[] consumed) {
-        Log.d(TAG , "onNestedPreScroll ---dy: " + dy);
-        //consumed[1]初始值为0
-        if (dy != 0 && !mSkipNestedPreScroll) {
-            int min, max;
-            if (dy < 0 && mChildFirstItemFullVisible) {
-                // We're scrolling down
-                if(mChildFirstItemFullVisible) {
-
-                }
-                min = 0;
-                max = getHeight();
-
-                Log.d(TAG , "onNestedPreScroll ---scroll down " + dy);
-                consumed[1] = mScrollerHelper.scroll(dy, min, max);
-            } else if(dy > 0){
-                // We're scrolling up
-                if(mChildLastItemFullVisible) {
-
-                }
-                if(mChildFirstItemFullVisible) {
-                    //先向下滑动，再向上滑动
-                }
-                min = -getHeight();
-                max = 0;
-
-                Log.d(TAG , "onNestedPreScroll ---scroll up " + dy);
-                consumed[1] = mScrollerHelper.scroll(dy, min, max);
-            }
-
-//            consumed[1] = mScrollerHelper.scroll(dy, min, max);
-        }
-    }
-
-    @Override
-    public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        Log.d(TAG, "onNestedScroll  [" + dxConsumed + "," + dyConsumed + "," + dxUnconsumed + "," + dyUnconsumed + "]");
-//        super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-    }
-
-    @Override
-    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
-        return true;
-//        return super.onNestedFling(target, velocityX, velocityY, consumed);
-    }
-
-    @Override
-    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
-        return true;
-//        return super.onNestedPreFling(target, velocityX, velocityY);
-    }
-
-    @Override
-    public void onStopNestedScroll(View child) {
-        Log.d(TAG , "onStopNestedScroll ----------");
-        mNestedScrollingParentHelper.onStopNestedScroll(child);
-        super.onStopNestedScroll(child);
-    }
-
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        mContentView = findViewById(R.id.dd_refresh_content);
-        if (mContentView == null) {
-            throw new RuntimeException("content must not null");
-        }
-        mHeaderView = findViewById(R.id.dd_refresh_header);
-        mFooterView = findViewById(R.id.dd_refresh_footer);
+    void setState(int state) {
+        this.mState = state;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        //measure content
-        final View content = mContentView;
-        measureChild(content, widthMeasureSpec, heightMeasureSpec);
-        //measure header
-        if (mHeaderView != null) {
-            final View header = mHeaderView;
-            measureChild(header, widthMeasureSpec, heightMeasureSpec);
-            mHeaderHeight = header.getMeasuredHeight();
+        if(mPullCriticalValue < getHeaderHeight()) {
+            mPullCriticalValue = getHeaderHeight();
         }
-
-        //measure footer
-        if (mFooterView != null) {
-            final View footer = mFooterView;
-            measureChild(footer, widthMeasureSpec, heightMeasureSpec);
-            mFooterHeight = footer.getMeasuredHeight();
+        if(mLoadCriticalValue < getFooterHeight()) {
+            mLoadCriticalValue = getFooterHeight();
         }
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int width = getWidth();
-        int height = getHeight();
-
-        //layout header
-        if (mHeaderView != null) {
-            mHeaderView.layout(0, -mHeaderHeight, width, 0);
-        }
-
-        //layout content
-        mContentView.layout(0, 0, width, height);
-
-        //layout footer
-        if (mFooterView != null) {
-            mFooterView.layout(0, height, width, height + mFooterHeight);
-        }
+        super.onLayout(changed, l, t, r, b);
     }
 
     @Override
-    protected LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
-        return new LayoutParams(p);
+    void onCurrentOffsetChange(int offset) {
+        if(offset == 0) {
+            setState(STATE_ORIGIN);
+        } else if(offset > 0) {
+            if(isNestedScrollStart()) {
+                //正在下拉
+                setState(STATE_PULL_DRAG);
+                return;
+            }
+            if(offset > mPullCriticalValue) {
+                setState(STATE_PULL_RETURN);
+            } else if(offset == mPullCriticalValue) {
+                setState(STATE_PULL_WAIT);
+            } else {
+                setState(STATE_PULL_RESET);
+            }
+        } else {
+            if(isNestedScrollStart()) {
+                //正在下拉
+                setState(STATE_LOAD_DRAG);
+                return;
+            }
+            if(-offset > mLoadCriticalValue) {
+                setState(STATE_LOAD_RETURN);
+            } else if(-offset == mLoadCriticalValue) {
+                setState(STATE_LOAD_WAIT);
+            } else {
+                setState(STATE_LOAD_RESET);
+            }
+        }
+    }
+
+    private boolean isReturnOrResetState() {
+        return mState > STATE_PULL_DRAG ||
+                mState < STATE_LOAD_DRAG;
     }
 
     @Override
-    public LayoutParams generateLayoutParams(AttributeSet attrs) {
-        return new LayoutParams(getContext(), attrs);
+    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+        boolean start = super.onStartNestedScroll(child, target, nestedScrollAxes);
+        return start && !isReturnOrResetState();
     }
 
     @Override
-    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
-        return p instanceof LayoutParams;
+    public void onStopNestedScroll(View child) {
+        super.onStopNestedScroll(child);
+        int offset = getCurrentOffset();
+        if (offset != 0) {
+            if (Math.abs(offset) > mPullCriticalValue ||
+                    Math.abs(offset) > mLoadCriticalValue) {
+                int newOffset;
+                if (offset > 0) {
+                    //滚动到下拉刷新状态, 向上回滚所以是个负值
+                    newOffset = -(offset - getHeaderHeight());
+                } else {
+                    //滚动到加载更多状态
+                    newOffset = -offset - getFooterHeight();
+                }
+                autoScroll(newOffset);
+                return;
+            }
+            autoScroll(-offset);
+        }
     }
 
-    public static class LayoutParams extends MarginLayoutParams {
-
-        public LayoutParams(Context c, AttributeSet attrs) {
-            super(c, attrs);
-        }
-
-        public LayoutParams(ViewGroup.LayoutParams source) {
-            super(source);
-        }
+    private void logD(String text) {
+        Log.d(TAG, text);
     }
 }
